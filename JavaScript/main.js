b7214c9cc63cc30f90a108e3697b6779777f146d
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-10-16 13:56:57 +0900

    updated

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,17 +2,23 @@
 const preLoader = document.querySelector('.preLoader');
 const main = document.querySelector('.main');
 const sig = document.querySelector('.sig');
+const wellcome = document.querySelector('.wellcome');
 
+function wellCome(){
+  wellcome.style.opacity = '1';
+}
 function sigLoader(){
   sig.style.opacity = '1';
-
 }
 function timeLoader(){
   preLoader.style.opacity = '0';
   main.style.opacity = '1';    
-  main.style.transition = 'all 1s ease';
-
+  preLoader.style.zIndex = '0'
+  main.style.zIndex = '1'
 };
+setTimeout(()=>{
+  wellCome()
+},'3000')
 setTimeout(()=>{
     sigLoader()
 },'1000')
