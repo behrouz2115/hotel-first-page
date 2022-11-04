c4fdb2fa85d45e4779d41a1ce156f681da7b90d0
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-11-04 21:32:26 +0900

    footer updated

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -46,6 +46,30 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+var swiper_logos = new Swiper(".logos-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
 
 
 
